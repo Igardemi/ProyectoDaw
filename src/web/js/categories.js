fb8d955b7791ee6b32e2a8e3141fa16e3e9e9ca1
Igardemi <ival69@hotmail.com>
2022-12-16 23:01:07 +0100

    fiz(#15): font

--- src/web/js/categories.js
+++ src/web/js/categories.js
@@ -23,10 +23,10 @@ function getCategories(){
 }
 
 function getOption(opciones){
-    textInfo="<select id='opciones' name='opciones'>";
+    textInfo="<select id='opciones' name='opciones' style='height:40px;'>";
     let contador = 0;
     opciones.forEach(opcion => {
-        textInfo+=`<option value=${contador}>${opcion}</option>`;
+        textInfo+=`<option value="${contador}">${opcion}</option>`;
         contador++;
     })
     textInfo+="</select>";
@@ -34,18 +34,23 @@ function getOption(opciones){
 }
 
 function getProductos(object){
-    let innerText="<div class='l-flex l-flex--wrap'>";
+    console.log(object);
+    let innerText=`
+    <div class='cabecera'>
+    Opciones de movilidad en ${object.ciudad}
+    </div>
+    <div class='l-flex l-flex--wrap l-flex--gap-10 l-flex--justify-content-center'>`;
+     
 object.opciones.forEach(opcion => {
     innerText+=`
-    <div class='c-article g--margin-4'>
-    <div class='c-article__imagen g--margin-4'>
-    <img src="${opcion.image}" alt="foto ticket" width="200" height="160">
-    </div>
-    <div class='l-flex l-flex--direction-column g--margin-4'>
-    <div class='c-article__nombre g--margin-4'>${opcion.nombre}</div>
+    <div class='c-article g--padding-5'>
+    <img src="${opcion.image}" alt="foto ticket" width="200" height="200">
+    <div class='c-article__nombre'>${opcion.nombre}</div>
     <div class='c-article__duracion'>${getOption(opcion.duracion)}</div>
     <div class='c-article__precio'>${opcion.precio[0]} Euros</div>
-    <button class="c-button ${opcion.nombre} ${opcion.duracion} btnBuy">Comprar</button>   
+    <div class='l-flex l-flex--direction-row l-flex--gap-10'>
+    <button class="c-button c-article__button c-button--alternativo ${opcion.nombre} ${opcion.duracion} btnBuy">Comprar</button>
+    <button class="c-button c-article__button ${opcion.nombre} ${opcion.duracion} btnBuy">info</button>      
     </div>
     </div>`;   
 })
